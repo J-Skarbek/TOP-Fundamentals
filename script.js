d65dfd87cb5ab373116e8b472ad5ecd30a9d94ab
J-Skarbek <john@snarkmediaagency.com>
2022-06-24 13:03:51 -0500

    Remove old comments from js file and work on implmenting a 5 round game loop. Add new button to html file.

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ let playerSelect = ''
 let computersChoice = ''
 let result = ''
 
-const btn = document.querySelector('#btn');
+const btn = document.querySelector('#btn')
+const btn2 = document.querySelector('#btn2')
 
 let userChoice = () => {
     playerSelect = prompt("Rock, Paper, or Scissors")
     computerPlay()
     compare(playerSelect, computersChoice)
     alert(result)
+}
+
+let multigame = () => {
+    playerSelect = prompt("Rock, Paper, or Scissors")
+    computerPlay()
+    fiveRoundGame()
+    alert(result)
+    
 }
 
 let computerPlay = () => {
@@ -27,32 +36,33 @@ let compare = (playerSelect, computersChoice) => {
     if (playerSelect === 'Scissors' || playerSelect === 'Paper' || playerSelect === 'Rock') {
         if ( playerSelect === computersChoice ) {
             result = 'This is a Tie -- go again!'
-            // return 'This is a Tie -- go again!'
 
         } else if (playerSelect === 'Scissors' && computersChoice === 'Paper') {
             result = 'Computer loses -- Scissors cut Paper!'
-            // return 'Computer loses -- Scissors cut Paper!'
 
         } else if (playerSelect === 'Paper' && computersChoice === 'Rock') {
             result = 'Computer Loses -- Paper wraps Rock!'
-            // return 'Computer Loses -- Paper wraps Rock!'
 
         } else if (playerSelect === 'Rock' && computersChoice === 'Scissors') {
             result = 'Computer Loses -- Rock crushes Scissors!'
 
         } else if (playerSelect === 'Paper' && computersChoice === 'Scissors') {
             result = 'You Lose -- Scissors cut Paper!'
-            // return 'You Lose -- Scissors cut Paper!'
 
         } else if (playerSelect === 'Rock' && computersChoice === 'Paper') {
             result = 'You Lose -- Paper wraps Rock!'
-            // return 'You Lose -- Paper wraps Rock!'
 
         } else if (playerSelect === 'Scissors' && computersChoice === 'Rock') {
             result = 'You Lose -- Rock crushes Scissors!'
-            // return 'You Lose -- Rock crushes Scissors!'
         }        
     }
 }
 
+let fiveRoundGame = () => {
+    for (let i = 0; i < 5; i++) {
+        multigame()
+    }
+}
+
 btn.addEventListener('click', userChoice);
+btn2.addEventListener('click', multigame);
